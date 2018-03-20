2aeae3b265db157cbf9f28a784217fda9332b712
hulklee01 <sungdl93@gmail.com>
2018-03-20 21:37:12 +0900

    Spring-IoC
    Inversion of Control

--- src/main/java/com/springbook/biz/TVUser.java
+++ src/main/java/com/springbook/biz/TVUser.java
@@ -3,7 +3,7 @@ package com.springbook.biz;
 public class TVUser {
     public static void main (String[] args) {
         BeanFactory factory = new BeanFactory();
-    	TV tv = (TV)factory.getBean(args[0]);
+    	TV tv = (TV)factory.getBean("samsung");
         tv.powerOn();
         tv.powerOff();
         tv.volumeUp();
